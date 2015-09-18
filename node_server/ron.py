eabe4840483b98a5cdd157004d9a705fac15a8c1
ODROID <ronhandler@gmail.com>
2015-09-18 23:26:12 +0930

    Added a loop that saves images

--- node_server/ron.py
+++ node_server/ron.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
-import os.path
 import cv2
-import numpy as np
 import time
 
 
 cap = cv2.VideoCapture(0)
 ret, new_image = cap.read()
 cv2.imwrite('public/img.png', new_image)
-time.sleep(0.2)
+#time.sleep(0.01)
 
 #if cv2.waitKey(1000)==27:
 	#cap.release()
