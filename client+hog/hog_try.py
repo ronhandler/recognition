e8001722500167e07d5eb07241e27ba38e20f971
Mike <M-ik-e>
2015-09-19 19:13:59 +0300

    HELL YEAH!

--- client+hog/hog_try.py
+++ client+hog/hog_try.py
@@ -1,1 +1,24 @@
 #! /usr/bin/python
+
+import cv2
+import numpy as np
+
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+hogParams = {'winStride': (8, 8), 'padding': (32, 32), 'scale': 1.05}
+
+im = cv2.imread("img2.jpg")
+result = hog.detectMultiScale(im, **hogParams)
+r = result[0][0]
+print type(r)
+print r
+
+cv2.rectangle(im, (r[0],r[1]), (r[0]+r[2],r[1]+r[3]), (0,255,0), 5)
+
+cv2.namedWindow("people detector", 1)
+cv2.imshow("people detector", im)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+#h = hog.compute(im)
+#print type(h) 
