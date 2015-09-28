92ddd4a465e486eb97c7cc548ae0d00ba1030679
Mike <M-ik-e>
2015-09-28 21:28:21 +0300

    ...

--- temp_flask/camera.py
+++ temp_flask/camera.py
@@ -3,15 +3,15 @@ import cv2
 
 class Camera(object):
 
-	def __init__(self):
-		self.cap = cv2.VideoCapture(0)
+    def __init__(self):
+        self.cap = cv2.VideoCapture(0)
         self.cap.set(3,320)
         self.cap.set(4,240)
 
-	def __del__(self):
-		self.cap.release()
+    def __del__(self):
+        self.cap.release()
 
-	def get_frame(self):
-		success, image = self.cap.read()
-		ret, jpeg = cv2.imencode('.jpg', image)
-		return jpeg.tostring()
+    def get_frame(self):
+        success, image = self.cap.read()
+        ret, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tostring()
