0ad5ab9bda4946d6309003239531e463acf44cf2
Mike <M-ik-e>
2015-10-02 22:32:13 +0300

    Calibration is improved

--- node_server/calibrate/calibrate.py
+++ node_server/calibrate/calibrate.py
@@ -2,21 +2,53 @@
 import cv2
 import time
 import os, sys
+import numpy as np
+
+CAM_QUANTITY = 8
 
 def mouse_handler(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
-        print(str(x) + "," + str(y))
+        images[param] = np.copy(temp_images[param])
+        cv2.imshow("header"+str(param),images[param])
+        cv2.circle(images[param],(x,y),25,(255,0,0),3) 
     if event == cv2.EVENT_RBUTTONDOWN:
-        print("Right button")
+        images[param] = np.copy(temp_images[param])
+        cv2.imshow("header_x",images[param])
 
-img = cv2.imread("img.png")
-cv2.imshow("header", img)
-cv2.setMouseCallback("header", mouse_handler)
+def enter_handler():
+    x = raw_input("Please, enter the X!!!")
+    return x
+
+temp_images =[]
+images = []
+for i in range (0, CAM_QUANTITY):    
+    cv2.namedWindow("header"+str(i), cv2.WINDOW_OPENGL|cv2.WINDOW_AUTOSIZE)
+    cv2.setMouseCallback("header"+str(i), mouse_handler, i)
+
+for i in range (0, CAM_QUANTITY):
+    cap = cv2.VideoCapture("http://192.168.1.20"+str(i)+":800"+str(i)+"/img.png")
+    ret, img = cap.read()
+    temp_images.append(img)
+    images.append(img)
+    cap.release()
+
 while True:
+    for i in range (0, CAM_QUANTITY):
+        if images[i] is None:
+            continue
+        cv2.imshow("header"+str(i), images[i])
+    
     k = cv2.waitKey(33)
     if k==27:   # Esc key
         exit(0)
-    if k==10:   # Return key
-        pass
+    if k == 10 or k == 13:   # Return key
+        print "Enter"
+
+        y = enter_handler()
+        print y
     if k==-1:
         continue
+    #else:
+        #print k
+cv2.destroyAllWindows()
+cap.release()
--- node_server/ron_cap.py
+++ node_server/ron_cap.py
@@ -12,18 +12,18 @@ import threading
 from threading import Lock
 
 DEBUG_LEVEL = 0
+MAX_CAM_NUMBER = 8
 
 def url_to_image(url):
     # download the image, convert it to a NumPy array, and then read
     # it into OpenCV format
-    resp = urllib.urlopen(url)
-    image = np.asarray(bytearray(resp.read()), dtype="uint8")
-    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-    return image
-
-#def my_sig(signum, frame):
-    #print "### Received a signal"
-
+    try:
+        resp = urllib.urlopen(url)
+        image = np.asarray(bytearray(resp.read()), dtype="uint8")
+        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+        return image
+    except:
+        return None
 
 class capture(threading.Thread):
 
@@ -41,16 +41,12 @@ class capture(threading.Thread):
 
     def getHog(self):
         r = None
-        self.l.acquire()
         r = self.hog
-        self.l.release()
         return r
 
     def getImage(self):
         img = None
-        self.l.acquire()
         img = self.image
-        self.l.release()
         return img
 
     def run(self):
@@ -65,7 +61,6 @@ class capture(threading.Thread):
 
             # Run the hog algorithm to find the location of the human being.
             if image is not None:
-                #print "Image is not none", self.i
                 hog = self.h.hog_f(image)
                 self.l.acquire()
                 self.hog = hog
@@ -76,29 +71,27 @@ if __name__ == '__main__':
 
     try:
         lock = Lock()
-        for i in range(0,8):
+        for i in range(0, MAX_CAM_NUMBER):
             print i
             my_thread = capture(i, lock)
             my_thread.start()
             process_list.append(my_thread)
         while True:
-            for i in range(0,8):
+            loop_results = []
+            for i in range(0, MAX_CAM_NUMBER):
                 image = process_list[i].getImage()
                 hog = process_list[i].getHog()
-
+                loop_results.append((image, hog))
                 if image is not None:
                     if hog is not None:
                         r = hog
                         cv2.rectangle(image, (r[0],r[1]), (r[0]+r[2],r[1]+r[3]), (0,255,0), 5)
-                    print "imshow", i
                     cv2.imshow("people detector "+str(i), image)
                     cv2.waitKey(1)
-            pass
     except KeyboardInterrupt:
-        for i in range(0,8):
+        for i in range(0, MAX_CAM_NUMBER):
             print "Terminating process "+str(i)
             cv2.destroyAllWindows()
             process_list[i].stop()
-            #process_list[i].terminate()
         print "Exiting."
         sys.exit(1)
