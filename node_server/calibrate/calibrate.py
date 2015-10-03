780b83f2af30a82bf2d3e21e5a884871e4e3876d
ronhandler <ronhandler@gmail.com>
2015-10-03 17:31:49 +0300

    Changing some stuff

--- node_server/calibrate/calibrate.py
+++ node_server/calibrate/calibrate.py
@@ -13,10 +13,10 @@ def mouse_handler(event, x, y, flags, param):
         cv2.circle(images[param],(x,y),25,(255,0,0),3) 
     if event == cv2.EVENT_RBUTTONDOWN:
         images[param] = np.copy(temp_images[param])
-        cv2.imshow("header_x",images[param])
+        #cv2.imshow("header_x",images[param])
 
 def enter_handler():
-    x = raw_input("Please, enter the X!!!")
+    x = raw_input("Please, enter the X... ")
     return x
 
 
@@ -25,51 +25,64 @@ def pic_capture():
         cv2.namedWindow("header"+str(i), cv2.WINDOW_OPENGL|cv2.WINDOW_AUTOSIZE)
         cv2.setMouseCallback("header"+str(i), mouse_handler, i)
 
-    #for i in range (0, CAM_QUANTITY):
+    for i in range (0, CAM_QUANTITY):
         for j in range (0,3):
             cap = cv2.VideoCapture("http://192.168.1.20"+str(i)+":800"+str(i)+"/img.png")
             if cap is not None:
                 break
         ret, img = cap.read()
         if img is not None:
-            temp_images[i] = img
-            images[i] = img
+            temp_images[i] = np.copy(img)
+            images[i] = np.copy(img)
         cap.release()
+
+    for i in range (0, CAM_QUANTITY):
+        if images[i] is None:
+            continue
+        cv2.imshow("header"+str(i), images[i])
+
+#?#wp_cam_pos_center = [None]*
 
 temp_images = [None]*8
 images = [None]*8
-#?#wp_cam_pos_center = [None]*
 
+pic_capture()
 
 while True:
-    cv2.destroyAllWindows()
-    enter_handler()
-    pic_capture()
-    while True:
-        for i in range (0, CAM_QUANTITY):
-            if images[i] is None:
-                continue
-            cv2.imshow("header"+str(i), images[i])
-        
-        k = cv2.waitKey(33)
-        if k==27:   # Esc key
-            exit(0)
-        # if k == Enter
-        if k == 10 or k == 13:   # Return key
-            break
-        #if k == r
-        if k == 114:
-            #cv2.destroyAllWindows()
-            pic_capture()
+    for i in range (0, CAM_QUANTITY):
+        if images[i] is None:
             continue
-        #if k == s
-        if k == 115:
-            continue
-        
-        if k==-1:
-            continue
-        #else:
-            #print k
-cv2.destroyAllWindows()
+        cv2.imshow("header"+str(i), images[i])
+    
+    k = cv2.waitKey(33)
+    if k==27:   # Esc key
+        exit(0)
+    # if k == Enter
+    if k == 10 or k == 13:   # Return key
+        temp_images = [None]*8
+        images = [None]*8
+
+        cv2.destroyAllWindows()
+        for i in range (0,100):
+            cv2.waitKey(10)
+
+        #enter_handler()
+
+        temp_images = [None]*8
+        images = [None]*8
+        pic_capture()
+        continue
+    #if k == r
+    if k == 114:
+        pic_capture()
+        continue
+    #if k == s
+    if k == 115:
+        continue
+    
+    if k==-1:
+        continue
+    #else:
+        #print k
 
 cap.release()
