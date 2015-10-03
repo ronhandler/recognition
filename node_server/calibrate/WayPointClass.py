43db82d9e7ae4b028462e3d68cb7888ae2b91131
Mike <M-ik-e>
2015-10-03 19:38:08 +0300

    WayPoint in the development

--- node_server/calibrate/WayPointClass.py
+++ node_server/calibrate/WayPointClass.py
@@ -1,10 +1,10 @@
 #! /usr/bin/python
 
-def WayPoint(object):
-    def __init__(self, i, x, y):
+class WayPoint(object):
+    def __init__(self, i, p_x, p_y):
         self.cam_id = i
-        self.cam_pos = (x,y)
-        self.phys_pos = None
+        self.cam_pos = None
+        self.phys_pos = (p_x,p_y)
     
-    def set_phys_position(self, p_x, p_y):
-        self.phys_pos = (p_x, p_y)
+    def set_cam_position(self, c_x, c_y):
+        self.cam_pos = (c_x, c_y)
--- node_server/calibrate/calibrate.py
+++ node_server/calibrate/calibrate.py
@@ -3,27 +3,33 @@ import cv2
 import time
 import os, sys
 import numpy as np
+from WayPointClass import WayPoint
 
 CAM_QUANTITY = 8
 
 def mouse_handler(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
-        images[param] = np.copy(temp_images[param])
-        cv2.imshow("header"+str(param),images[param])
-        cv2.circle(images[param],(x,y),25,(255,0,0),3) 
+        images[param[0]] = np.copy(temp_images[param[0]])
+        cv2.imshow("header"+str(param[0]),images[param[0]])
+        cv2.circle(images[param[0]],(x,y),25,(255,0,0),3) 
+        if param[1] is not None and param[2] is not None:
+            wp = WayPoint(param[0], param[1], param[2])
+            #print wp
+            wp.set_cam_position(x,y)
+            temp_WP[param[0]] = wp
     if event == cv2.EVENT_RBUTTONDOWN:
-        images[param] = np.copy(temp_images[param])
+        images[param[0]] = np.copy(temp_images[param[0]])
         #cv2.imshow("header_x",images[param])
 
 def enter_handler():
-    x = raw_input("Please, enter the X... ")
+    x = raw_input("Please, enter the physical position:").split(',')
     return x
 
 
-def pic_capture():
-    for i in range (0, CAM_QUANTITY):    
-        cv2.namedWindow("header"+str(i), cv2.WINDOW_OPENGL|cv2.WINDOW_AUTOSIZE)
-        cv2.setMouseCallback("header"+str(i), mouse_handler, i)
+def pic_capture(phy_p_x, phy_p_y):
+    #for i in range (0, CAM_QUANTITY):    
+        #cv2.namedWindow("header"+str(i), cv2.WINDOW_OPENGL|cv2.WINDOW_AUTOSIZE)
+        #cv2.setMouseCallback("header"+str(i), mouse_handler, i)
 
     for i in range (0, CAM_QUANTITY):
         for j in range (0,3):
@@ -40,13 +46,16 @@ def pic_capture():
         if images[i] is None:
             continue
         cv2.imshow("header"+str(i), images[i])
+        cv2.setMouseCallback("header"+str(i), mouse_handler, (i,phy_p_x,phy_p_y))
 
 #?#wp_cam_pos_center = [None]*
 
 temp_images = [None]*8
 images = [None]*8
+temp_WP = [None]*8
+WP = [None]*8
 
-pic_capture()
+pic_capture(None, None)
 
 while True:
     for i in range (0, CAM_QUANTITY):
@@ -58,31 +67,32 @@ while True:
     if k==27:   # Esc key
         exit(0)
     # if k == Enter
-    if k == 10 or k == 13:   # Return key
-        temp_images = [None]*8
-        images = [None]*8
+    if k == 105:   # Return key
 
         cv2.destroyAllWindows()
-        for i in range (0,100):
-            cv2.waitKey(10)
+        #for i in range (0,1000):
+            #cv2.waitKey(10)
 
-        #enter_handler()
-
-        temp_images = [None]*8
-        images = [None]*8
-        pic_capture()
+        coord = enter_handler()
+        coord_x = int(coord[0])
+        coord_y = int(coord[1])
+        pic_capture(coord_x, coord_y)
         continue
     #if k == r
     if k == 114:
-        pic_capture()
+        print "R is pressed"
+        pic_capture(None, None)
         continue
     #if k == s
     if k == 115:
+        WP = temp_WP
+        for i in WP:
+            print i
         continue
     
     if k==-1:
         continue
-    #else:
-        #print k
+    else:
+        print k
 
 cap.release()
