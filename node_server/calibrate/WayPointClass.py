7715de9c42e1096e035bfb5010eccbe74ae7ce5e
ronhandler <ronhandler@gmail.com>
2015-10-03 21:02:32 +0300

    Now saving coords properly in cal.py

--- node_server/calibrate/WayPointClass.py
+++ node_server/calibrate/WayPointClass.py
@@ -1,10 +1,13 @@
 #! /usr/bin/python
 
 class WayPoint(object):
-    def __init__(self, i, p_x, p_y):
-        self.cam_id = i
+    def __init__(self):
+        self.cam_id = -1
         self.cam_pos = None
-        self.phys_pos = (p_x,p_y)
-    
-    def set_cam_position(self, c_x, c_y):
-        self.cam_pos = (c_x, c_y)
+        self.phys_pos = None
+    def __str__(self):
+        s = ""
+        s += "    cam_id:" + str(self.cam_id) + "\n" 
+        s += "    cam_pos:" + str(self.cam_pos) + "\n"
+        s += "    phys_pos:" + str(self.phys_pos)
+        return s
