4c319223f6aa0236f8b1ec7cf8df80fbc5244303
Mike <M-ik-e>
2015-10-19 04:11:16 +0300

    ...

--- release/server/svm_try/svm_learning.py
+++ release/server/svm_try/svm_learning.py
@@ -4,11 +4,25 @@ import cv2
 import numpy as np
 import xml.etree.ElementTree as ET
 
-descs = None
-resps = None
+from PIL import Image
+import os, os.path
+
+DIR = './Neg' 
+images = []
+for i in range(0,len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])):
+    png = Image.open("./Neg/neg"+str(i)+".png")
+    images.append(np.array(png))
+
+
+svm_params = dict( kernel_type = cv2.SVM_LINEAR, 
+                       svm_type = cv2.SVM_C_SVC,
+                       C = 1 )
+
+descs = np.copy(images)
+resps = np.copy(images)
 
 svm = cv2.SVM()
-svm.train_auto(descs, resps, None, None, params=cv2.svm_params, k_fold=5)
+svm.train_auto(descs, resps, None, None, params=svm_params, k_fold=5)
 
 svm.save("svm.xml")
 tree = ET.parse('svm.xml')
