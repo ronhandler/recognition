c8eea72930a24d18b88a2a8e6d4d3bbc8132fbc6
Mike <M-ik-e>
2015-10-25 17:28:34 +0200

    one server with diff and one with hog

--- node_server/hog_try.py
+++ node_server/hog_try.py
@@ -2,17 +2,27 @@
 
 import cv2
 import numpy as np
+from ron_cap import dist
 
 class Hog_D(object):
     def __init__(self):
         self.hog = cv2.HOGDescriptor()
         self.hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
         self.hogParams = {'winStride': (4, 4), 'padding': (32, 32), 'scale': 1.05}
+        self.black_list = []
 
+    def build_black_list(self):
+        result, w = self.hog.detectMultiScale(im, **(self.hogParams))
+        for res in result:
+            if res is not in black_list:
+                self.black_list.append(res) 
 
-    def hog_f(self, im):
-        result = self.hog.detectMultiScale(im, **(self.hogParams))
+    def get(self, im):
+        result, w = self.hog.detectMultiScale(im, **(self.hogParams))
         r = None
-        if len(result[0])>0 :
-            r = result[0][0]
+        if len(result[0])>0:
+            for res in result:
+                for b in black_list:
+                    if dist(b, res) > 5:
+                        r = res
         return r
