728587b2759976842d0bbbb94860966fad77f7ca
ronhandler <ronhandler@gmail.com>
2015-11-20 17:28:55 +0200

    Plot can now draw a path real time

--- release/plot.py
+++ release/plot.py
@@ -5,7 +5,7 @@ import time
 import cv2
 import sys
 
-img = cv2.imread("map.png")
+img = cv2.imread("map_big.png")
 
 i = 0
 try:
@@ -13,7 +13,8 @@ try:
         cv2.line(img,(0+i,0),(50,110),(255,0,0),2)
         cv2.line(img,(50+i,110),(200,100),(255,255,0),2)
         cv2.imshow("title", img)
-        k = cv2.waitKey(500)
+        k = cv2.waitKey(50)
+        i = i+1
 
 
 except KeyboardInterrupt:
